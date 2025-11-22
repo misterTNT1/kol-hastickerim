547d21987477545f0b8e6e65f7b9adc6198cb9c8
eyal3 <011eyalm@shamir.school>
2025-11-22 22:57:05 +0200

    added the functions that handle uploading a story and searching one

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const button = document.querySelector("button");
+const submitButton = document.getElementById("submit");
+const searchButton = document.getElementById("search") 
 
-
-function upload_image(){
-    let htmlCode = document.createElement("div");
-    htmlCode.innerHTML = `<p>this is a sample text</p>`;
-    document.body.appendChild(htmlCode);
+function uploadStory(){
+    const name = document.getElementById("name");
+    const story = document.getElementById("story");
+    const stories = JSON.parse(localStorage.getItem("stories")) || {};
+    stories[name.value] = story.value;
     
+    localStorage.setItem("stories", JSON.stringify(stories));
 }
 
-button.addEventListener("click", () => upload_image());
+function search(){
+    const stories = JSON.parse(localStorage.getItem("stories")) || {};
+    const name = document.getElementById("name");
+    if (name in stories){
+        window.location.href = `story.html?id=${encodeURIComponent(name)}`;
+    }
+    else{
+        alert("story not found");
+    }
+}
 
+
+submitButton.addEventListener("click", uploadStory);
+searchButton.addEventListener("click", search);
